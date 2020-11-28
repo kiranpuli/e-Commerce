e15eb2f7d0fec5a9e056e2e631ac3e70ec953b1d
kiranpuli <kiranpuli070899@gmail.com>
2020-11-28 14:09:15 +0530

    created product component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
+import React,{Component} from "react"
 import './bootstrap.min.css'
 import "./App.css"
-function App() {
-  return (
-    <div className="App">
-      <nav className="navbar navbar-dark bg-dark">
-          <a href="/" className="navbar-brand">ShopNow</a>
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <a href="/" className="nav-link">Admin</a>
-            </li>
-          </ul>
-      </nav>
-      <main>
-        This is the product list
-      </main>
-      <footer className="bg-dark text-light">
-        Made by Kiran Puli, Copyright @2020
-      </footer>
-    </div>
-  );
+
+import data from "./data.json"
+import Products from "./components/Products";
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      products:data.products,
+      size:"",
+      sort:""
+    }
+  }
+  
+  render(){
+    console.log(data.products)
+      return (
+        <div className="App container-fluid">
+          <nav className="navbar navbar-dark bg-dark">
+              <a href="/" className="navbar-brand">ShopNow</a>
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <a href="/" className="nav-link">Admin</a>
+                </li>
+              </ul>
+          </nav>
+          <main>
+            <div className="main">
+              <Products products={this.state.products}/>
+            </div>
+            <div className="sidebar">Cart</div>
+          </main>
+          <footer className="bg-dark text-light">
+            Made by Kiran Puli, Copyright @2020
+          </footer>
+        </div>
+      );
+  }
 }
 
 export default App;
