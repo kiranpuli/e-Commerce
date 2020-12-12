a1c882851724e173498b19d8ebb1d0c62556aeb8
kiranpuli <kiranpuli070899@gmail.com>
2020-12-12 20:29:51 +0530

    :sparkles: added product filters

--- src/App.js
+++ src/App.js
@@ -1,42 +1,95 @@
-import React,{Component} from "react"
-import './bootstrap.min.css'
-import "./App.css"
+import React, { Component } from "react";
+import "./bootstrap.min.css";
+import "./App.css";
 
-import data from "./data.json"
+import data from "./data.json";
 import Products from "./components/Products";
+import Filter from "./components/Filter";
+
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      products:data.products,
-      size:"",
-      sort:""
-    }
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
   }
-  
-  render(){
-    console.log(data.products)
-      return (
-        <div className="App container-fluid">
-          <nav className="navbar navbar-dark bg-dark">
-              <a href="/" className="navbar-brand">ShopNow</a>
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <a href="/" className="nav-link">Admin</a>
-                </li>
-              </ul>
-          </nav>
-          <main>
-            <div className="main">
-              <Products products={this.state.products}/>
-            </div>
-            <div className="sidebar">Cart</div>
-          </main>
-          <footer className="bg-dark text-light">
-            Made by Kiran Puli, Copyright @2020
-          </footer>
-        </div>
-      );
+
+  filterSort = (e) => {
+    const sort = e.target.value;
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          sort === "highest"
+            ? a.price <= b.price
+              ? 1
+              : -1
+            : sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : a._id > b._id
+            ? 1
+            : -1
+        ),
+    }));
+  };
+
+  filterSize = (e) => {
+    const size = e.target.value;
+    if (size === "") {
+      this.setState({
+        products: data.products,
+        size,
+      });
+    } else {
+      this.setState({
+        products: data.products.filter(
+          (e) => e.availableSizes.indexOf(size) >= 0
+        ),
+        size,
+      });
+    }
+  };
+
+  render() {
+    // console.log(data.products);
+    return (
+      <div className="App container-fluid">
+        <nav className="navbar navbar-dark bg-dark">
+          <a href="/" className="navbar-brand">
+            ShopNow
+          </a>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a href="/" className="nav-link">
+                Admin
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          <div className="main">
+            <Filter
+              count={this.state.products.length}
+              sort={this.state.sort}
+              size={this.state.size}
+              filterSort={this.filterSort}
+              filterSize={this.filterSize}
+            />
+            <Products products={this.state.products} />
+          </div>
+
+          <div className="sidebar">Cart</div>
+        </main>
+        <footer className="bg-dark text-light">
+          Made by Kiran Puli, Copyright @2020
+        </footer>
+      </div>
+    );
   }
 }
 
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,24 +1,29 @@
-import React, { Component } from 'react'
-import "../App.css"
+import React, { Component } from "react";
+import "../App.css";
 export default class Products extends Component {
-    render() {
-        return (
-            <div>
-                <ul className="products row">
-                    {
-                        this.props.products.map(e=>(
-                            <a href={"#"+e._id} class="card m-1" style={{width: "18rem"}}>
-                                <img class="card-img-top" src={e.image} alt={e.title}/>
-                                <div class="card-body">
-                                    <h5 class="card-text">{e.title}</h5>
-                                    <h5>{"$"+e.price}</h5><span><button className="btn btn-success">Add to cart</button></span>
-                                </div>
-                            </a>
-                        ))
-                    }
-                    
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <ul className="products row">
+          {this.props.products.map((e) => (
+            <a
+              key={e._id}
+              href={"#" + e._id}
+              className="card m-1"
+              style={{ width: "18rem" }}
+            >
+              <img className="card-img-top" src={e.image} alt={e.title} />
+              <div className="card-body">
+                <h5 className="card-text">{e.title}</h5>
+                <h5>{"$" + e.price}</h5>
+                <span>
+                  <button className="btn btn-success">Add to cart</button>
+                </span>
+              </div>
+            </a>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
